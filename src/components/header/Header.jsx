81bde611856adb1660a68d4d8a453c9de34d4e26
Nick Levshin <nikita.levshin2016@gmail.com>
2022-06-08 22:57:57 +0300

    feat: add react-icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './Header.sass';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Nikita Levshin</h1>
+        <h5 className="text-lights">Juniour Front-End Delveloper</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contacts" className="scrall__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
