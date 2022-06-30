3d71ca282a907c11f07e5b961620dd49ce763835
Nick Levshin <nikita.levshin2016@gmail.com>
2022-07-01 00:17:01 +0300

    feat: add contacts component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import './Footer.sass';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <a href="#header" className="footer__logo">
+        NICK-LEVSHIN
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Design. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
